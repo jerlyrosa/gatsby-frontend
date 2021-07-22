0b607f0abe121c331efc1ba3c8943f70029d6d24
sr-jerly <jerlymigueldelarosah@gmail.com>
2021-07-22 13:19:06 -0400

    Actualizando gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      // resolve: `gatsby-plugin-styled-components`,
+      // options: {
+      //   // Add any options here
+      // },
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
@@ -22,18 +28,18 @@ module.exports = {
 
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   // options: {
-    //   //   name: `gatsby-starter-default`,
-    //   //   short_name: `starter`,
-    //   //   start_url: `/`,
-    //   //   background_color: `#663399`,
-    //   //   theme_color: `#663399`,
-    //   //   display: `minimal-ui`,
-    //   //   // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   // },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
